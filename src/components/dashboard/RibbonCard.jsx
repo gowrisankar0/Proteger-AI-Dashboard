b3d788a4a598b9ae8be6e1c565b6fb1dde321746
GOWRI SANKAR <113678338+gowrisankar0@users.noreply.github.com>
2025-12-23 22:23:07 +0530

    Update RibbonCard.jsx

--- src/components/dashboard/RibbonCard.jsx
+++ src/components/dashboard/RibbonCard.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 export function RibbonCard({ title, value, colorClass }) {
     return (
         <div className={`${colorClass} relative px-6 py-8 rounded-xl text-white overflow-hidden shadow-sm flex flex-col justify-center min-h-[140px]`}>
-            {/* Background design elements - Overlapping Circles */}
+            
             <div className="absolute right-0 top-0 h-full w-2/3 overflow-hidden pointer-events-none">
-                {/* Large circle bottom right */}
+            
                 <div className="absolute -right-2 -bottom-10 w-48 h-48 bg-white/10 rounded-full blur-[1px]"></div>
                 <div className="absolute -right-2 -bottom-10 w-48 h-48 border border-white/20 rounded-full"></div>
 
-                {/* Smaller intersecting circle top right */}
+            
                 <div className="absolute right-12 -top-6 w-36 h-36 bg-white/5 rounded-full blur-[1px]"></div>
                 <div className="absolute right-12 -top-6 w-36 h-36 border border-white/10 rounded-full"></div>
             </div>
