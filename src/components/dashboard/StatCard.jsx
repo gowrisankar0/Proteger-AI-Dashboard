5e56197016f7fa14d014125c08d24c5020a3bd15
GOWRI SANKAR <113678338+gowrisankar0@users.noreply.github.com>
2025-12-23 22:23:53 +0530

    Update StatCard.jsx

--- src/components/dashboard/StatCard.jsx
+++ src/components/dashboard/StatCard.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export function StatCard({ title, items }) {
     return (
         <div className="bg-white rounded-xl border border-gray-200 overflow-hidden shadow-md flex flex-col h-full p-0">
-            {/* Header Section */}
+            
             <div className="bg-[#D4EBFF] border-b border-blue-100 px-3 py-2">
                 <h3 className="font-bold text-slate-800 text-lg">{title}</h3>
             </div>
 
-            {/* Body Section */}
+            
             <div className=" space-y-4 flex-1 px-3 py-3">
                 {items.map((item, idx) => (
                     <div key={idx} className="flex justify-between items-center text-sm">
